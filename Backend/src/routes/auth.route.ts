2d09a79aac60d249cf30b80ae0b9cab52200ce29
SarthakGupta345 <Sarthak987gupta654@gmail.com>
2026-01-10 11:47:42 +0530

    Auth Routes are added

---
+++ Backend/src/routes/auth.route.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import { generateLoginOTP, generateSignupOTP, Login, logout, Signup } from "../Controllers/auth.controller"
+
+const router = express.Router()
+
+router.post("/generateLoginOTP", generateLoginOTP)
+router.post("/login", Login);
+router.post('/generateSignupOTP', generateSignupOTP)
+router.post("/signup", Signup)
+router.post('/logout', logout)
+
+
+
+
+export default router
